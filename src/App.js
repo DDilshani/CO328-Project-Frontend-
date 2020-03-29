9503781c2773df167fbfcc38aa22f467debebf3e
DilshaniKarunarathna <dinelkadilshani95@gmail.com>
2020-03-29 22:48:31 +0530

    components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import '../node_modules/react';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
 import RegisterLanguage from "./components/registerLanguage.component";
@@ -19,53 +17,14 @@ import ResetPW from "./components/resetPW.component";
 import Guide from "./components/guide.component";
 import Schedule from "./components/schedule.component";
 import Terms from "./components/terms.component";
+import Hotline from "./components/hotline.component";
+import Navigation from './components/layout/Navigation';
 
 function App() {
   return (<Router>
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/registerLanguage"}>RegisterLanguage</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/registerSubscription"}>RegisterSubscription</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/resetPWSendCode"}>ResetPWSendCode</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/resetCode"}>ResetCode</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/incorrectCode"}>IncorrectCode</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/resetPW"}>ResetPW</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/guide"}>Guide</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/schedule"}>Schedule</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/terms"}>Terms</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Navigation></Navigation>
+      
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
@@ -81,6 +40,7 @@ function App() {
             <Route path="/guide" component={Guide} />
             <Route path="/schedule" component={Schedule} />
             <Route path="/terms" component={Terms} />
+            <Route path="/hotline" component={Hotline} />
           </Switch>
         </div>
       </div>
@@ -89,4 +49,3 @@ function App() {
 }
 
 export default App;
-
