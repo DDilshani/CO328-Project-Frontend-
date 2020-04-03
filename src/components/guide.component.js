c7848a915508412881e76a9dc2d4f4bcea5e3a84
DilshaniKarunarathna <dinelkadilshani95@gmail.com>
2020-04-03 18:13:33 +0530

    new

---
+++ src/components/guide.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class Guide extends Component {
+    
+
+    render() {
+        return (
+            <form>
+                <h3>Guide to Register</h3>
+
+                <p className="forgot-password text-center">
+                    Register with the mobile platform as......... and we will send you a token. 
+                </p>
+                <br></br>
+
+                <div className="form-group">
+                    <label>Token</label>
+                    <input type="text" className="form-control" placeholder="Enter the token" />
+                </div>
+
+                <button type="back" className="btn btn-success btn-block">Register</button>
+            </form>
+        );
+    }
+}
---
+++ src/components/registration_components/UserFunctions.js
@@ -0,0 +1,62 @@
+import axios from 'axios'
+
+export const register = newCustomer => {
+  return axios
+    .post('customers/register', {
+      firstName: newCustomer.firstName,
+      lastName: newCustomer.lastName,
+      phoneNo: newCustomer.phoneNo,
+      email: newCustomer.email,
+      address1: newCustomer.address1,
+      address2: newCustomer.address2,
+      city: newCustomer.city,
+      regDate: newCustomer.regDate,
+      password: newCustomer.password,
+      customerType: newCustomer.customerType,
+      language: newCustomer.language,
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+export const login = input => {
+    let newCustomer = input.customer;
+    let rememberMe = input.rememberMe;
+
+    return axios
+    .post('customers/login', {
+      phoneNo: newCustomer.phoneNo,
+      email: newCustomer.email,
+      password: newCustomer.password
+    })
+    .then(response => {
+        localStorage.setItem('usertoken', response.data.sessionToken);
+        return response.data
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+export const devVerification = input => {
+
+  console.log(input);
+
+  return axios
+  .post('api/v1/verifyToken/', {
+    accessToken: 'f83bdbecf8f2596cfd837b11ab2aa1fb',
+    userToken: input.token,
+    userTele: input.phoneNo
+  })
+  .then(response => {
+      console.log(response.data)
+      return response.data
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
