b33514fc96b75a963274f09b0f7aa80e785883e2
Pasan <pasan96tennakoon@gmail.com>
2020-04-10 20:19:44 +0530

    deploye erros fixed

---
+++ build/precache-manifest.d9a36bc667fc6e1cf2a91f97abbea7ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a1e8fc27cfc40703082636e25dab0a",
+    "url": "/CO328-Project-Frontend-/index.html"
+  },
+  {
+    "revision": "07732713eccbb900a879",
+    "url": "/CO328-Project-Frontend-/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "84d7ccc5a430c8a8f418",
+    "url": "/CO328-Project-Frontend-/static/css/main.da27a40f.chunk.css"
+  },
+  {
+    "revision": "07732713eccbb900a879",
+    "url": "/CO328-Project-Frontend-/static/js/2.849e94fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CO328-Project-Frontend-/static/js/2.849e94fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d7ccc5a430c8a8f418",
+    "url": "/CO328-Project-Frontend-/static/js/main.b40f6603.chunk.js"
+  },
+  {
+    "revision": "49045966d8d5888924d2",
+    "url": "/CO328-Project-Frontend-/static/js/runtime-main.dafc0b15.js"
+  }
+]);
