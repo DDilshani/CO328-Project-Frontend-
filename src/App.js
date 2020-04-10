a03222d05cec1e0904f798ca31daa60b75e32622
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-04-10 18:21:20 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navigationbar from './components/layout/Navigationbar'
-import Login from './components/Login'
-import Register from './components/Register'
-import Landing from './components/Landing'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Landing from './components/pages/Landing'
 
 class App extends Component {
   render() {
---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = global.config = {
+   lang: {
+      welcome: {
+         en: "",
+      }
+   },
+   telco:{
+      tokenVerify: "http://collector.ceykod.com/api/v1/verifyToken/"
+   },
+   hostURL: "http://collectorapp.ceykod.com"
+};
