d665cbf52c4b233fa57099e7e8f52ebb4f57397d
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-05-25 11:11:31 +0530

    .

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ import Dashboard from './components/dashboard_components/Dashboard';
 import NewPickup from './components/dashboard_components/newPickup';
 
 class App extends Component {
+
+   componentDidMount(){
+      // Redirect to requested URL 
+     const queryString = window.location.search;
+     const urlParams = new URLSearchParams(queryString);
+     const page = urlParams.get('p')
+     console.log(page);
+
+     if(page!=null){
+        this.props.history.push(page);
+     }
+  }
+
    render() {
       return (
           <Router basename={process.env.PUBLIC_URL}>
